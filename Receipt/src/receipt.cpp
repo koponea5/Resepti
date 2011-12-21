f9939125d5681d9ec312377d92275458d6bf78ec
Koponen Aila <koponea5@cc.hut.fi>
2011-12-21 16:07:28 +0200

    exercise ready!

--- Receipt/src/receipt.cpp
+++ Receipt/src/receipt.cpp
@@ -4,13 +4,29 @@
  *  Created on: 21.12.2011
  *      Author: koponea5
  */
-#include "receipt.h"
+
 #include <string>
 #include <iostream>
 #include <cstdlib>
 #include <vector>
+#include "receipt.h"
 
 using namespace std;
+
+class receipt {
+	vector<Product> vee;
+
+public:
+	receipt();
+	receipt(const Product &product);
+	receipt(std::vector<Product> product);
+	~receipt();
+	void add(const Product &product);
+	void remove();
+	void print();
+	void receiptTest();
+};
+
 
 receipt::receipt(const std::vector<Product> products) {
 	this->vee = products;
@@ -30,10 +46,14 @@ void receipt::add(const Product &pr) {
 //	pr.price = tuotedatat[1];
 //	pr.quantity = tuotedatat[2];
 	this->vee.push_back(pr);
+    cout << "Added: " << pr.quantity << " x " << pr.name << " a " << pr.price << " eur " << pr.quantity*pr.price << endl;
 }
 void receipt::remove() {
     if (!(this->vee.empty())) {
-    	this->vee.pop_back();
+    vector<Product>::iterator it;
+    it=vee.end()-1;
+    cout << "Removed: " << it->quantity << " x " << it->name << " a " << it->price << " eur " << it->quantity*it->price << endl;
+   	this->vee.pop_back();
     }
     else {
     	cout << "Error: nothing to remove" << endl;
@@ -55,14 +75,18 @@ void receipt::print() {
 
 
 //testataan reseptia
-
-int main(void) {
+void receiptTest() {
+	string prods = "jeppe 1.0 5";
     Product prod;
     Product prod2;
     prod.name = "Teppo";
     prod.price = 15.1;
     prod.quantity = 4;
-    prod2 = {"seppo", 11.1, 1};
+    prod2.name = "seppo";
+    prod2.price = 11.1;
+    prod2.quantity = 1;
+    Product prod3;
+
     vector<Product> testVector;
 
     testVector.push_back(prod);
@@ -82,7 +106,36 @@ int main(void) {
     rese.remove();
     rese.remove();
 
+}
 
+int main(void) {
 
+	//receiptTest();
+	receipt res1;
+	Product prod;
+	string str;
+	int i = 1;
+	cout << "COMMANDS: ADD, REMOVE, PRINT, QUIT" << endl;
+	while(i==1) {
+		cout << "Enter command:" << endl;
+		cin >> str;
+		if(str=="REMOVE") {
+			res1.remove();
+		} else if(str=="QUIT") {
+			i=0;
+		} else if(str=="ADD") {
+			cout << "Enter product information (<name> <price> <quantity>):" << endl;
+			cin >> prod.name;
+			cin >> prod.price;
+			cin >> prod.quantity;
+			res1.add(prod);
+			//luetaan!!
+		} else if(str=="PRINT") {
+			res1.print();
+		} else {
+			cout << "Error: Invalid command" << endl;
+		}
+
+	}
 	return 0;
 }
--- Receipt/src/receipt.h
+++ Receipt/src/receipt.h
@@ -2,11 +2,6 @@
 #define ELEC_AS_CPP_U1E1
 
 #include <string>
-#include <iostream>
-#include "receipt.h"
-#include <vector>
-
-using namespace std;
 
 /* Product structure */
 struct Product
@@ -15,25 +10,6 @@ struct Product
 	double price;
     int quantity;
 };
-
-class receipt {
-	vector<Product> vee;
-
-public:
-	receipt();
-	receipt(const Product &product);
-	receipt(std::vector<Product> product);
-	~receipt();
-	void add(const Product &product);
-	void remove();
-	void print();
-};
-//const char *receipt::talk() {
-//	int sentence = rand() % repertoire.size();
-//	return repertoire[sentence].c_str();
-//}
-
-
 
 
 
